10fd88b68521be811ac65743ab8438218762eccf
fdfcosta-telecon <fabricio.costa@teleconsistemas.com.br>
2023-02-20 13:17:49 -0300

    Configurando EFCore, DI e DbContext

---
+++ eCommerce.API/Database/eCommerceContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace eCommerce.API.Database
+{
+    public class eCommerceContext : DbContext
+    {
+        public eCommerceContext(DbContextOptions<eCommerceContext> options) : base(options)
+        {
+
+        }
+    }
+}
